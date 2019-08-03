5c7a7dde77772e0c4e3a470c2e8c87e6943ec502
kunsay <kunsay0326@hotmail.co.jp>
2019-08-03 12:29:57 +0900

    make miniApp name=Fuwafuwa
    ミニアプリケーションを作成しました。

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,45 @@
+class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
+  def index
+    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(text: tweet_params[:text], user_id: current_user.id)
+    redirect_to action: :index
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+    redirect_to action: :index
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+    redirect_to action: :index
+  end
+  
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+end
